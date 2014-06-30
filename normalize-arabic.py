39f2484356b335b8d0339f64249cf329f523d86f
Waleed Ammar <wammar@cs.cmu.edu>
2014-06-29 20:15:33 -0400

    Update normalize-arabic.py

--- normalize-arabic.py
+++ normalize-arabic.py
@@ -9,11 +9,12 @@ arg_parser.add_argument("-i", "--input_filename", required=True, help="normal ar
 arg_parser.add_argument("-o", "--output_filename", required=True, help="normalized arabic")
 args = arg_parser.parse_args()
 
-kashida_re = re.compile(u'DISABLED')# re.compile(u'[ـ]')
-haa_re = re.compile(u'DISABLED')# re.compile(u'[هة]')
-yaa_re = re.compile(u'DISABLED')# re.compile(u'[يى]')
-alef_re = re.compile(u'DISABLED')# re.compile(u'[أإآ]')
-vowels_re = re.compile(u'')
+kashida_re = re.compile(u'[ـ]')
+haa_re =  re.compile(u'[هة]')
+yaa_re =  re.compile(u'[يى]')
+alef_re =  re.compile(u'[أإآ]')
+# http://symbolcodes.tlt.psu.edu/bylanguage/arabicchart.html#vowel
+vowels_re = re.compile(u'[\u1611]')
 with io.open(args.output_filename, encoding='utf8', mode='w') as output_file:
   for line in io.open(args.input_filename, encoding='utf8'):
     line, _ = re.subn(alef_re, u'ا', line)
