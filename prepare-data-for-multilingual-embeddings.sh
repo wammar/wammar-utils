445ab9f955a42f547b811e765936e1e28e53c30c
Waleed Ammar <wammar@cs.cmu.edu>
2015-11-30 15:35:57 -0500

    modify how we filter wikipedia text

--- prepare-data-for-multilingual-embeddings.sh
+++ prepare-data-for-multilingual-embeddings.sh
@@ -10,12 +10,12 @@ do
   awk '$3 > 0.25 {print $1,"|||",$2}' align.${lang}-en > dict.${lang}-en
 done
 
-# use 10 million sentences from the wikipedia dump of each language
+# use up to 10 million sentences from the wikipedia dump of each language
 for lang in bg cs da de el en es fi fr hu it sv
 do
   wget http://download.wikimedia.org/${lang}wiki/latest/${lang}wiki-latest-pages-articles.xml.bz2
   python ~/wammar-utils/WikiExtractor.py --processes 1 -b 1G -o extracted-${lang} ${lang}wiki-latest-pages-articles.xml.bz2
-  find extracted-${lang} -name 'wiki_*' -print0 | xargs -0 -I file cat file | egrep -v "^<" | egrep "." | ~/wammar-utils/europarl-tools/tokenizer.perl -l ${lang} | head -10000000 > mono.${lang}.cased
+  find extracted-${lang} -name 'wiki_*' -print0 | xargs -0 -I file cat file | egrep -v "^[ ^I]*<" | grep "[^ ]" | ~/wammar-utils/europarl-tools/tokenizer.perl -l ${lang} | head -10000000 > mono.${lang}.cased
   python ~/wammar-utils/lowercase.py -i mono.${lang}.cased -o mono.${lang}
 
   # remove temporary files (uncomment the following line to save disk space)
