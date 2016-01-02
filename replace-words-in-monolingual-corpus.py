6607ac7662af02081f84cdb7e66266d77d6774ed
wammar <waleed.ammar@gmail.com>
2016-01-01 20:38:35 -0800

    improve the way we handle cluster mappings

--- replace-words-in-monolingual-corpus.py
+++ replace-words-in-monolingual-corpus.py
@@ -12,29 +12,28 @@ from collections import defaultdict
 # parse/validate arguments
 #example invocation:   python $wammar_utils/replace-words-in-monolingual-corpus.py -d $word_clusters -l "de:" "en:" "es:" "fr:" "id:" "it:" "ja:" "ko:" "pt:" "sv:" -i $corpus_de $corpus_en $corpus_es $corpus_fr $corpus_id $corpus_it $corpus_ja $corpus_ko $corpus_pt $corpus_sv -o corpus.langprefix -od $augmented_word_clusters
 argparser = argparse.ArgumentParser()
-argparser.add_argument("-d", "--input-dictionary-filename", help=" Input dictionary that maps from words to word clusters. Each line specifies a unique word and what it should map to; e.g., 'en:dog ||| clusterid13458'")
-argparser.add_argument("-od", "--output-dictionary-filename", help=" Output dictionary that maps from words to trivial (singleton) word clusters. Each line specifies a unique word and what it should map to; e.g., 'en:newdog ||| clusterid134513'")
+argparser.add_argument("-c", "--clusters-filename", help=" Each line specifies the cluster of one word, and is formatted as three tab-delimited strings: cluster_id, word, and an obsolete frequency value; e.g., 'clusterid13458   en:dog  10'. The cluster information are first read from this file, then extended to include new words in the monolingual corpus, then written back to the same file.")
 argparser.add_argument("-l", "--language-prefixes", nargs="+", action="append", help=" List of strings to prefix each word in the corresponding monolingual corpus. ")
 argparser.add_argument("-i", "--input-filenames", nargs="+", action="append", help=" List of input monolingual corpora. Must be of the same length as the list of language prefixes")
-argparser.add_argument("-o", "--output-filename", help=" Aggregate text in the input monolingual files, but each word in the dictionary file is replaced by its corresponding clusterid and prefixed with language id. ")
+argparser.add_argument("-o", "--output-filename", help=" Aggregate text in the input monolingual files, but each word in the clusters file is replaced by its corresponding clusterid and prefixed with language id. ")
 args = argparser.parse_args()
 
-# read dictionary
+# read clusters
 max_cluster_id = -1
-with io.open(args.input_dictionary_filename, encoding='utf8', mode='r') as dictionary_file:
-  dictionary_items = {}
-  for line in dictionary_file:
-    splits = line.split(' ||| ')
-    assert len(splits) == 2
-    if splits[0].strip() in dictionary_items:
-      print u"WARNING: a word appears twice at the left side of the dictionary. I'll use the older mapping and discard the new one."
-      #Old mapping is {}. New mapping is {}. I will ignore the new mapping and keep the old one.".format(splits[0].strip(), dictionary_items[splits[0].strip()], splits[1].strip())
+with io.open(args.clusters_filename, encoding='utf8', mode='r') as clusters_file:
+  word2cluster = {}
+  for line in clusters_file:
+    splits = line.split('\t')
+    assert len(splits) == 3
+    cluster, word, dummy = splits
+    if word in word2cluster:
+      print u"WARNING: a word appears twice in the clusters file. I'll use the older mapping and discard the new one."
       continue
-    dictionary_items[splits[0].strip()] = splits[1].strip()
-    assert(splits[1][0:9] == "clusterid")
-    cluster_id = int(splits[1].strip()[9:])
+    word2cluster[word] = cluster
+    assert(cluster[0:9] == "clusterid")
+    cluster_id = int(cluster[9:])
     if cluster_id > max_cluster_id: max_cluster_id = cluster_id
-  print 'read {} dictionary items.'.format(len(dictionary_items))
+  print 'read {} words from the clusters file.'.format(len(word2cluster))
 
 language_corpus_tuples = []
 for language_prefix, corpus_filename in zip(args.language_prefixes[0],args.input_filenames[0]):
@@ -42,26 +41,27 @@ for language_prefix, corpus_filename in zip(args.language_prefixes[0],args.input
 
 output_file = io.open(args.output_filename, encoding='utf8', mode='w')
 for (language_prefix, input_file) in language_corpus_tuples:
-  lines_counter, tokens_counter, replaces_counter = 0, 0, 0
+  lines_counter, tokens_counter = 0, 0
   for line in input_file:
     lines_counter += 1
-    splits = line.strip().split()
+    splits = line.strip().split(' ')
     for i in xrange(len(splits)):
       tokens_counter += 1
       splits[i] = language_prefix + splits[i]
-      if splits[i] in dictionary_items:
-        replaces_counter += 1
-        splits[i] = dictionary_items[splits[i]]
+      if splits[i] in word2cluster:
+        splits[i] = word2cluster[splits[i]]
       else:
         max_cluster_id += 1
         cluster_id_string = u'clusterid{}'.format(max_cluster_id)
-        dictionary_items[splits[i]] = cluster_id_string
+        word2cluster[splits[i]] = cluster_id_string
         splits[i] = cluster_id_string
     output_file.write(u'{}\n'.format(' '.join(splits)))
-  print language_prefix, '{} lines read.\n{}% ({} out of {}) tokens replaced.'.format(lines_counter, 100.0 * replaces_counter / tokens_counter, replaces_counter, tokens_counter)
+  print language_prefix, '{} lines read. {} tokens replaced.'.format(lines_counter, tokens_counter)
 output_file.close()
 
 # write dictionary
-with io.open(args.output_dictionary_filename, encoding='utf8', mode='w') as dictionary_file:
-  for word in dictionary_items.keys():
-    dictionary_file.write(u'{} ||| {}\n'.format(word, dictionary_items[word]))
+with io.open(args.clusters_filename, encoding='utf8', mode='w') as dictionary_file:
+  for word in word2cluster.keys():
+    dictionary_file.write(u'{}\t{}\t10\n'.format(word2cluster[word], word))
+
+print 'wrote {} words to the clusters file.'.format(len(word2cluster))
