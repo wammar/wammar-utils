ffb37d6f8d15a53a06fac79d6f9ef237819f49fd
wammar <wammar@cs.cmu.edu>
2016-05-04 03:05:48 -0400

    refactor gardner et al

--- DXDsvd.m
+++ DXDsvd.m
@@ -1,9 +1,11 @@
 % adapted from the original implementation of Gardner et al. (EMNLP 2015).
 function DXDsvd(r, input_mat_filename, temp1_filename, temp2_filename, output_mat_filename, varargin)
+  r = str2num(r)
+
   lam = 1;
 
   % the matrix D is written only once for all languages
-  load input_mat_filename
+  load(input_mat_filename)
   D2 = D;
   D1 = D;
 
@@ -11,13 +13,13 @@ function DXDsvd(r, input_mat_filename, temp1_filename, temp2_filename, output_ma
   nVarargs = length(varargin);
   fprintf('Number of languages: %d.\n',nVarargs)
   for k = 1:nVarargs
-    cooccurence_filename = sprintf('%s%s', input_mat_filename, varargin{k})
-    load cooccurence_filename
-    if exist('total','var')
-      total = X;
-    else
-      total = total + X;
-    end
+	    cooccurence_filename = sprintf('%s.%s.mat', input_mat_filename, varargin{k})
+	    load(cooccurence_filename)
+	    if exist('total','var')
+	      total = total + X;
+	    else
+              total = X;
+            end
   end
 
   % done reading all cooccurence matrices
@@ -33,13 +35,15 @@ function DXDsvd(r, input_mat_filename, temp1_filename, temp2_filename, output_ma
   opts.issym = 1;
   opts.isreal= 1;
   tic
-  [ Q, D ] = eigs( @(u)DXDu( X, P1, P2, u, m, n ), m+n, 2*r, 'LM', opts );
+  disp(isscalar(2*r))
+  [ Q, D ] = eigs( @(u)DXDu( u, X, P1, P2, m, n ), m+n, 2*r, 'LM', opts );
+
   Ss = D(1:r,1:r);
   Vs = sqrt(2)*Q(1:n,1:r);
   Us = sqrt(2)*Q(n+1:n+m,1:r);
   t = toc;
-  save(temp2_filename, 'nnzX', 'nnzD1', 'nnzD2', 't');
-  save(temp1_filename, 'Us', 'Ss', 'Vs', 'lam')
+  %save(temp2_filename, 'nnzX', 'nnzD1', 'nnzD2', 't');
+  save(temp1_filename, 'Us', 'Ss', 'Vs', 'lam');
 
   % We actually only use 'Us' and scipy/octave handle structs badly
   % so save 'Us' separately as ascii to be loaded with np.loadtxt
@@ -47,9 +51,9 @@ function DXDsvd(r, input_mat_filename, temp1_filename, temp2_filename, output_ma
 exit;
 end
 
-function u = DXDu( X, P1, P2, u, m, n )
+function output = DXDu( u, X, P1, P2, m, n )
   u1 = u(1:n); u2 = u(n+1:m+n);
   uu1 = P2* ( X'* (P1'*u2) );
   uu2 = P1* ( X * (P2'*u1) );
-  u = [ uu1; uu2 ];
+  output = [ uu1; uu2 ];
 end
--- gardner-et-al-emnlp-2015.py
+++ gardner-et-al-emnlp-2015.py
@@ -11,64 +11,9 @@ from scipy.sparse import coo_matrix
 from scipy.io import savemat, loadmat
 from math import log
 import numpy as np
+import random
 
-# parse/validate arguments
-argparser = argparse.ArgumentParser(description="This is my implementation of the translation-invariance method for estimating multilingual word embeddings, \n"
-                                    + "proposed by Gardner et al. (EMNLP 2015). \n"
-                                    + "If you use this implementation, please cite both Gardner et al. (2015) and http://arxiv.org/abs/1602.01925 \n"
-                                    + "\n"
-                                    + "Sample invocation: python gardner-et-al-emnlp-2015.py --corpus multilingual-corpus.prefixed.txt --alignments aligned-words.prob --min_frequency 35 --window 3 --embeddings output-file.embeddings")
-argparser.add_argument("-c", "--corpus", required=True, help="(input) multilingual corpus")
-argparser.add_argument("-a", "--alignments", required= True, help="(input) alignments probabilities")
-argparser.add_argument("-f", "--min_frequency", type=int, default=5, help="(hyperparameter) minimum frequency for a word to be included in the vocabulary")
-argparser.add_argument("-w", "--window", type=int, default=3, help="(hyperparameter) distance between two cooccuring words in a sentence")
-argparser.add_argument("-s", "--embedding_size", default=40, help="(hyperparameter) size of each word embedding vector in the output file.")
-argparser.add_argument("-e", "--embeddings", required=True, help="(output) embeddings file")
-args = argparser.parse_args()
-
-# compute word frequencies
-tokens_counter = 0
-word2freq = defaultdict(float)
-with io.open(args.corpus, encoding='utf8') as corpus:
-  for line in corpus:
-    tokens = line.strip().split(' ')
-    for token in tokens:
-      word2freq[token] += 1
-      tokens_counter += 1
-print 'corpus has', tokens_counter, 'tokens, and', len(word2freq), 'word types'
-
-# filter out low frequency words from the vocabulary, and give each word a unique id
-id2word = []
-word2id = {}
-id2freq = []
-total_freq = 0
-for word in word2freq.keys():
-  freq = word2freq[word] * 1.0
-  if freq < args.min_frequency: 
-    del word2freq[word]
-    continue
-  word_id = len(id2word)
-  word2id[word] = word_id
-  id2word.append(word)
-  freq *= args.window * 2.0
-  id2freq.append(freq)
-  total_freq += freq
-  del word2freq[word]
-# add sentence boundary words
-SOS, EOS = u'<s>', u'</s>'
-if SOS not in word2id:
-  word2id[SOS] = len(id2word)
-  id2word.append(SOS)
-  id2freq.append(0.0)
-if EOS not in word2id:
-  word2id[EOS] = len(id2word)
-  id2word.append(EOS)
-  id2freq.append(0.0)
-SOS_ID, EOS_ID = word2id[SOS], word2id[EOS]
-print 'after excluding infrequent words, vocabulary size =', len(id2word)
-assert len(id2freq) == len(id2word)
-
-def write_x(word2context2freq, lang, any_any2freq, id2freq, id2word):
+def write_x(word2context2freq, lang, any_any2freq, id2freq, id2word, input_mat_filename):
   # create three lists that summarize the data: row_ids, column_ids, pmis
   row_ids, column_ids, pmis = [], [], []
   for row_id in xrange(len(word2context2freq)):
@@ -90,117 +35,192 @@ def write_x(word2context2freq, lang, any_any2freq, id2freq, id2word):
   pmis, row_ids, column_ids = None, None, None
 
   # save X in matlab format (bad filename, please change this later)
-  input_mat_filename_per_lang = 'multilingual.D.mat.{}'.format(lang)
+  input_mat_filename_per_lang = '{}.{}'.format(input_mat_filename, lang)
   savemat(input_mat_filename_per_lang, dict(X=X))
   print 'wrote the matrix X of language {} to {}'.format(lang, input_mat_filename_per_lang)
   X = None
 
-# compute cooccurence statistics
-word2context2freq = [defaultdict(float) for i in xrange(len(id2word))]
-any_any2freq = total_freq
-window_offsets = range(-args.window, 0) + range(1, args.window+1)
-sys.stdout.write('lines processed:\n')
-current_lang = ''
-all_langs = set()
-with io.open(args.corpus, encoding='utf8') as corpus:
-  lines_counter = 0
-  for line in corpus:
-    lines_counter += 1
-    if lines_counter % 100000 == 0: sys.stdout.write('{}\n'.format(lines_counter))
-    if line[0:2] != current_lang:
-      if current_lang != '': 
-        all_langs.add(current_lang)
-        write_x(word2context2freq, current_lang, any_any2freq, id2freq, id2word)
-        # reset memory
-        word2context2freq = None
-        word2context2freq = [defaultdict(float) for i in xrange(len(id2word))]
-      current_lang = line[0:2]
-    id2freq[SOS_ID] += 2.0 * args.window
-    id2freq[EOS_ID] += 2.0 * args.window
-    # read a sentence
-    splits = line.strip().split(' ')
-    word_ids = []
-    # first, insert SOS tokens
-    for i in xrange(args.window):
-      word_ids.append(SOS_ID)
-    # then add frequent words
-    for token in splits:
-      if token not in word2id: continue
-      word_ids.append(word2id[token])
-    # then add EOS tokens
-    for i in xrange(args.window):
-      word_ids.append(EOS_ID)
-    # update word_context2freq
-    for token_index in xrange(args.window, len(word_ids) - args.window):
-      focus_word_id = word_ids[token_index]
-      for context_offset in window_offsets:
-        context_word_id = word_ids[token_index + context_offset]
-        word2context2freq[focus_word_id][context_word_id] += 1
+# parse/validate arguments
+argparser = argparse.ArgumentParser(description="This is my implementation of the translation-invariance method for estimating multilingual word embeddings, \n"
+                                    + "proposed by Gardner et al. (EMNLP 2015). \n"
+                                    + "If you use this implementation, please cite both Gardner et al. (2015) and http://arxiv.org/abs/1602.01925 \n"
+                                    + "\n"
+                                    + "Sample invocation: python gardner-et-al-emnlp-2015.py --corpus multilingual-corpus.prefixed.txt --alignments aligned-words.prob --min_frequency 35 --window 3 --embeddings output-file.embeddings")
+argparser.add_argument("-c", "--corpus", help="(input) multilingual corpus -- required unless -U is specified.")
+argparser.add_argument("-a", "--alignments", help="(input) alignments probabilities -- required unless -U is specified.")
+argparser.add_argument("-U", "--intermediate_u", required=False, help="(input) when this argument is specified, we skip all the steps of computing the embeddings, " + 
+                       "including the call to matlab, and use the file specified here as the output of the DXDsvd matlab module. This is mainly provided for debugging purposes.")
+argparser.add_argument("-I", "--intermediate_stats", required=False, help="(input) when this argument is specified, we skip the expensive computation of input matrices that encode monolingual context pointwise mutual information as well as bilingual relationships. This is mainly provided for debugging purposes.")
+argparser.add_argument("-v", "--vocab_filename", help="(input) required if -U is specified.")
+argparser.add_argument("-f", "--min_frequency", type=int, default=5, help="(hyperparameter) minimum frequency for a word to be included in the vocabulary")
+argparser.add_argument("-w", "--window", type=int, default=3, help="(hyperparameter) distance between two cooccuring words in a sentence")
+argparser.add_argument("-s", "--embedding_size", default=40, help="(hyperparameter) size of each word embedding vector in the output file.")
+argparser.add_argument("-e", "--embeddings", required=True, help="(output) embeddings file")
+args = argparser.parse_args()
 
-  # write the matrix for the last language
-  write_x(word2context2freq, current_lang, any_any2freq, id2freq, id2word)
-print 'done counting in word2context2freq!!!\n'
-print 'observed languages are: {}'.format(', '.join(all_langs))
+if (args.intermediate_u):
+  output_mat_filename = args.intermediate_u
+  vocab_filename = args.vocab_filename
+else:
+  input_mat_filename = str(random.randint(10000, 99999))
 
-# we no longer need the dictionary
-word2context2freq = None
+  # compute word frequencies
+  tokens_counter = 0
+  word2freq = defaultdict(float)
+  with io.open(args.corpus, encoding='utf8') as corpus: 
+    for line in corpus:
+      tokens = line.strip().split(' ')
+      for token in tokens:
+        word2freq[token] += 1
+        tokens_counter += 1
+  print 'corpus has', tokens_counter, 'tokens, and', len(word2freq), 'word types'
 
-# save the word ids to interpret rows and columns of the matrix
-vocab_filename = 'multilingual.vocab'
-with io.open(vocab_filename, encoding='utf8', mode='w') as vocab:
-  for i in xrange(len(id2word)):
-    vocab.write(u'{} {}\n'.format(i, id2word[i]))
+  # filter out low frequency words from the vocabulary, and give each word a unique id
+  id2word = []
+  word2id = {}
+  id2freq = []
+  total_freq = 0
+  for word in word2freq.keys():
+    freq = word2freq[word] * 1.0
+    if freq < args.min_frequency: 
+      del word2freq[word]
+      continue
+    word_id = len(id2word)
+    word2id[word] = word_id
+    id2word.append(word)
+    freq *= args.window * 2.0
+    id2freq.append(freq)
+    total_freq += freq
+    del word2freq[word]
+  # add sentence boundary words
+  SOS, EOS = u'<s>', u'</s>'
+  if SOS not in word2id:
+    word2id[SOS] = len(id2word)
+    id2word.append(SOS)
+    id2freq.append(0.0)
+  if EOS not in word2id:
+    word2id[EOS] = len(id2word)
+    id2word.append(EOS)
+    id2freq.append(0.0)
+  SOS_ID, EOS_ID = word2id[SOS], word2id[EOS]
+  print 'after excluding infrequent words, vocabulary size =', len(id2word)
+  assert len(id2freq) == len(id2word)
+  
+  # compute cooccurence statistics
+  word2context2freq = [defaultdict(float) for i in xrange(len(id2word))]
+  any_any2freq = total_freq
+  window_offsets = range(-args.window, 0) + range(1, args.window+1)
+  sys.stdout.write('lines processed:\n')
+  current_lang = ''
+  all_langs = set()
+  with io.open(args.corpus, encoding='utf8') as corpus:
+    lines_counter = 0
+    for line in corpus:
+      lines_counter += 1
+      if lines_counter % 100000 == 0: sys.stdout.write('{}\n'.format(lines_counter))
+      if line[0:2] != current_lang:
+        all_langs.add(line[0:2])
+        if current_lang != '': 
+          current_lang = line[0:2]
+          if args.intermediate_stats: continue
+          write_x(word2context2freq, current_lang, any_any2freq, id2freq, id2word, input_mat_filename)
+          # reset memory
+          word2context2freq = None
+          word2context2freq = [defaultdict(float) for i in xrange(len(id2word))]
+        current_lang = line[0:2]
 
-# read alignment probabilities
-src_ids, tgt_ids, probs = [], [], []
-srcid2total = [0.0 for i in xrange(len(id2word))]
-print 'file=',args.alignments
-with io.open(args.alignments, encoding='utf8', mode='r') as alignments:
-  for line in alignments:
-    src, tgt, prob = line.strip().split(' ')
-    prob = float(prob)
-    # skip tiny probabilities
-    if prob < 0.001: continue
-    # skip infrequent words
-    if src not in word2id or tgt not in word2id: continue
-    # update the lists which will be later used to initialize a sparse scipy matrix
-    src_id, tgt_id = word2id[src], word2id[tgt]
-    # forward direction
-    src_ids.append(src_id)
-    tgt_ids.append(tgt_id)
-    probs.append(prob)
-    srcid2total[src_id] += prob
-# normalize
-for i in xrange(len(src_ids)):
-  src_id = src_ids[i]
-  probs[i] /= srcid2total[src_id]
-print 'number of nonzero alignment probabilities in the {}x{} translation matrix is {}'.format(len(id2word), len(id2word), len(probs))
-
-# create the sparse matrix that represents translations
-print len(probs), len(src_ids), len(tgt_ids)
-print probs[:10], src_ids[:10], tgt_ids[:10]
-print probs[-10:], src_ids[-10:], tgt_ids[-10:]
-D1 = coo_matrix((probs, (src_ids, tgt_ids)), shape=(len(id2word), len(id2word)))
-# we no longer need the lists
-probs, src_ids, tgt_ids = None, None, None
-
-# save matrices in matlab format
-input_mat_filename = 'multilingual.D.mat'
-savemat(input_mat_filename, dict(D=D1))
-print 'wrote the matrix D1 to ', input_mat_filename
-
-# optimize using matlab. this function writes the output to temporary files which are then read by the following lines in this script.
-print "Calling matlab's function DXDsvd() ..."
-temp_filenames = ['gardner-et-al-emnlp-2015.temp1', 'gardner-et-al-emnlp-2015.temp2']
-output_mat_filename = 'DXDsvd{}lam1_ascii_Us.mat'.format(args.embedding_size)
-command_line = 'matlab --eval "DXDsvd({}, \'{}\', \'{}\', \'{}\', \'{}\', {})"'.format(args.embedding_size, 
-                                                                                       input_mat_filename, 
-                                                                                       temp_filenames[0], 
-                                                                                       temp_filenames[1], 
-                                                                                       output_mat_filename,
-                                                                                       ', '.join(["'{}'".format(lang) for lang in all_langs]))
-subprocess.call([command_line], shell=True)
-print "Done.\n"
+      if args.intermediate_stats: continue
+      id2freq[SOS_ID] += 2.0 * args.window
+      id2freq[EOS_ID] += 2.0 * args.window
+      # read a sentence
+      splits = line.strip().split(' ')
+      word_ids = []
+      # first, insert SOS tokens
+      for i in xrange(args.window):
+        word_ids.append(SOS_ID)
+      # then add frequent words
+      for token in splits:
+        if token not in word2id: continue
+        word_ids.append(word2id[token])
+      # then add EOS tokens
+      for i in xrange(args.window):
+        word_ids.append(EOS_ID)
+      # update word_context2freq
+      for token_index in xrange(args.window, len(word_ids) - args.window):
+        focus_word_id = word_ids[token_index]
+        for context_offset in window_offsets:
+          context_word_id = word_ids[token_index + context_offset]
+          word2context2freq[focus_word_id][context_word_id] += 1
+  
+    # write the matrix for the last language
+    if not args.intermediate_stats: write_x(word2context2freq, current_lang, any_any2freq, id2freq, id2word)
+  print 'done counting in word2context2freq!!!\n'
+  print 'observed languages are: {}'.format(', '.join(all_langs))
+  
+  # we no longer need the dictionary
+  word2context2freq = None
+  
+  # save the word ids to interpret rows and columns of the matrix
+  if args.intermediate_stats:
+    vocab_filename = args.vocab_filename
+  else:
+    vocab_filename = 'multilingual-{}langs.vocab'.format('_'.join(all_langs))
+    with io.open(vocab_filename, encoding='utf8', mode='w') as vocab:
+      for i in xrange(len(id2word)):
+        vocab.write(u'{} {}\n'.format(i, id2word[i]))
+  
+  # read alignment probabilities
+  if args.intermediate_stats:
+    input_mat_filename = args.intermediate_stats
+  else:
+    src_ids, tgt_ids, probs = [], [], []
+    srcid2total = [0.0 for i in xrange(len(id2word))]
+    print 'file=',args.alignments
+    with io.open(args.alignments, encoding='utf8', mode='r') as alignments:
+      for line in alignments:
+        src, tgt, prob = line.strip().split(' ')
+        prob = float(prob)
+        # skip tiny probabilities
+        if prob < 0.001: continue
+        # skip infrequent words
+        if src not in word2id or tgt not in word2id: continue
+        # update the lists which will be later used to initialize a sparse scipy matrix
+        src_id, tgt_id = word2id[src], word2id[tgt]
+        # forward direction
+        src_ids.append(src_id)
+        tgt_ids.append(tgt_id)
+        probs.append(prob)
+        srcid2total[src_id] += prob
+    # normalize
+    for i in xrange(len(src_ids)):
+      src_id = src_ids[i]
+      probs[i] /= srcid2total[src_id]
+    print 'number of nonzero alignment probabilities in the {}x{} translation matrix is {}'.format(len(id2word), len(id2word), len(probs))
+    
+    # create the sparse matrix that represents translations
+    print len(probs), len(src_ids), len(tgt_ids)
+    print probs[:10], src_ids[:10], tgt_ids[:10]
+    print probs[-10:], src_ids[-10:], tgt_ids[-10:]
+    D1 = coo_matrix((probs, (src_ids, tgt_ids)), shape=(len(id2word), len(id2word)))
+    # we no longer need the lists
+    probs, src_ids, tgt_ids = None, None, None
+    
+    # save matrices in matlab format
+    savemat(input_mat_filename, dict(D=D1))
+    print 'wrote the matrix D1 to ', input_mat_filename
+    
+  # optimize using matlab. this function writes the output to temporary files which are then read by the following lines in this script.
+  print "Calling matlab's function DXDsvd() ..."
+  temp_filenames = ['gardner-et-al-emnlp-2015.temp1', 'gardner-et-al-emnlp-2015.temp2']
+  output_mat_filename = 'DXDsvd{}lam1_ascii_Us.mat'.format(args.embedding_size)
+  command_line = 'matlab -r "DXDsvd {} \'{}\' \'{}\' \'{}\' \'{}\' {}"'.format(args.embedding_size, 
+                                                                                         input_mat_filename, 
+                                                                                         temp_filenames[0], 
+                                                                                         temp_filenames[1], 
+                                                                                         output_mat_filename,
+                                                                                         ' '.join(["'{}'".format(lang) for lang in all_langs]))
+  subprocess.call([command_line], shell=True)
+  print "Done.\n"
 
 # now read the matrix Us from the output file DXDsvd40lam1.mat
 Us = np.loadtxt(output_mat_filename)
