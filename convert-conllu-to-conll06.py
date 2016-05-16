b1acfcc1d6ead6de93d501a215d79dfec419aec0
wammar <wammar@cs.cmu.edu>
2016-05-16 19:59:09 -0400

    arbitrarily fix circular dependencies in conll treebank files

--- convert-conllu-to-conll06.py
+++ convert-conllu-to-conll06.py
@@ -8,6 +8,15 @@ argparser.add_argument("-i", "--input_filename", required=True)
 argparser.add_argument("-o", "--output_filename", required=True)
 args = argparser.parse_args()
 
+def is_cyclic(child_to_parent_cache, child_key):
+  current_parent = child_to_parent_cache[child_key]
+  while True:
+    if current_parent not in child_to_parent_cache: return False
+    if current_parent == child_key: return True
+    current_parent = child_to_parent_cache[current_parent]
+
+sents_counter = 0
+child_to_parent_cache = {}
 with io.open(args.input_filename) as input_file:
   with io.open(args.output_filename, mode='w') as output_file: 
     current_sentence = []
@@ -19,10 +28,21 @@ with io.open(args.input_filename) as input_file:
       elif len(line.strip()) == 0 and len(current_sentence) > 0:
         output_file.write(u''.join(current_sentence) + u'\n')
         current_sentence = []
+        child_to_parent_cache = {}
+        sents_counter += 1
       else:
         conll_fields = line.strip().split('\t')
         # Skip lines which describe multiple words since each of the individual words has a separate line.
         if '-' in conll_fields[0]: continue
+        # projected treebanks sometimes use head=-1 to indicate (multiple) roots
+        if int(conll_fields[6]) < 0: conll_fields[6] = "0"
+        # detect (some) circular dependencies and break them by assuming the later one is rooted at 0
+        child_to_parent_cache[conll_fields[0]] = conll_fields[6]
+        if is_cyclic(child_to_parent_cache, conll_fields[0]): 
+          print 'WARNING: sentence #', sents_counter, 'has a cycle. child_to_parent_cache is:\n', child_to_parent_cache
+          conll_fields[6] = "0"
+          child_to_parent_cache[conll_fields[0]] = conll_fields[6]
+          print 'to resolve this, i updated child_to_parent_cache as follows:\n', child_to_parent_cache, '\n'
         # remove language specific extensions of dependency relationships.
         if ':' in conll_fields[7]: conll_fields[7] = conll_fields[7][:conll_fields[7].find(':')]
         # replace - with _ in POS fields
